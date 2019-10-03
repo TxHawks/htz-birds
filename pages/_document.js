aa306d7b9fb93ed6225de746875413d6a92709bb
TxHawks <tofu.hawks@gmail.com>
2019-10-03 16:34:28 +0300

    add facebook and analytics scripts

--- pages/_document.js
+++ pages/_document.js
@@ -165,7 +165,6 @@ export default class CustomDocument extends Document {
           <React.Fragment>
             <script
               type="text/javascript"
-              // eslint-disable-next-line react/no-danger
               dangerouslySetInnerHTML={{
                 __html: `
               var _sf_async_config = _sf_async_config || {};
@@ -193,6 +192,29 @@ export default class CustomDocument extends Document {
               }}
             />
           </React.Fragment>
+
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '801998859871552');fbq('track', 'PageView');
+            `,
+            }}
+          />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-589309-3', 'auto');
+            ga('send', 'pageview');
+            `,
+            }}
+          />
         </CustomHead>
         <body>
           <Main />
