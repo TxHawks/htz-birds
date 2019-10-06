3b40dcf7c2117318b3fdc1df9d070ffc548d535b
TxHawks <tofu.hawks@gmail.com>
2019-10-06 16:09:28 +0300

    add purchase banner analytics actions

--- js/src/index.js
+++ js/src/index.js
@@ -7,10 +7,33 @@ export function init() {
   initActionBar();
   initAudio();
 
+  const purchaseBtn = document.getElementById('purchaseBtn')
+
   const isLoggedIn =
     document.cookie.split(';').some((item) => item.trim().startsWith('tmsso='));
   if (!isLoggedIn) {
     const loginLink = document.getElementById('loginLink');
     if (loginLink) loginLink.classList.add('isLoggedIn');
+  }
+
+  if (typeof ga === 'function') {
+    if (purchaseBtn) {
+      ga('ec:addPromo', {
+        id: 'interactive-birds',
+        name: 'interactive birds',
+      });
+
+      purchaseBtn.addEventListener('click', () => {
+        ga('ec:addPromo', {
+          id: 'interactive-birds',
+          name: 'interactive birds',
+        });
+        ga('ec:setAction', 'promo_click');
+        ga('send', 'event', 'Internal Promotions', 'click', 'interactive birds');
+      });
+
+      ga('send', 'pageview');
+    }
+    else ga('send', 'pageview');
   }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -210,8 +210,8 @@ export default class CustomDocument extends Document {
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-            ga('create', 'UA-589309-3', 'auto');
-            ga('send', 'pageview');
+            ga('create', 'UA-589309-3', 'auto', { allowLinker: true });
+            ga('require', 'ec');
             `,
             }}
           />
